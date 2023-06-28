990edc5f12143404e7be261b29977c599451ccab
Vladislav <vladislav.fedyukov@gmail.com>
2023-06-28 16:20:49 +0300

    Сохранение_2

--- DZ_4.py
+++ DZ_4.py
@@ -2,3 +2,16 @@
 # Определите какие вещи влезут в рюкзак передав его максимальную грузоподъёмность. 
 # Достаточно вернуть один допустимый вариант. *Верните все возможные варианты комплектации рюкзака.
 
+stuff = {'matches': 5, 'cup': 6, 'tent': 7, 'ration': 8, 'spare shoes': 4}
+
+def backpack_capacity(capacity: int, stuff: dict) -> list[str]:
+    packaging_option = []
+    summary = []
+    for key, value in stuff.items():
+        if value <= capacity:
+            capacity -= value
+            packaging_option.append(key)
+    return packaging_option
+
+
+print(backpack_capacity(15, stuff))
